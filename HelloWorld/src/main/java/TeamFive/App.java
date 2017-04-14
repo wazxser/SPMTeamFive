79c5c07ca0043d2b77fe6cbea0a3b9157cf4f29b
WangYuehuan <2386458640@qq.com>
2017-04-14 17:03:24 +0800

    test

---
+++ HelloWorld/src/main/java/TeamFive/App.java
@@ -0,0 +1,14 @@
+package TeamFive;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String prin()
+    {
+		String str = "Hello, WangYuehuan";
+        return str;
+    }
+}
---
+++ HelloWorld/src/test/java/TeamFive/AppTest.java
@@ -0,0 +1,16 @@
+package TeamFive;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	public App app = new App();
+	
+	@Test
+	public void testPrin(){
+		assertEquals("Hello, WangYuehuan", app.prin());
+	} 
+}
